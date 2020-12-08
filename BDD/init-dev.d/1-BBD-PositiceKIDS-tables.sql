4e546cd28cd1448a37ed28c7e35c00b2c99dfba6
tom.willemin <willemintom03@gmail.com>
2020-12-08 13:51:27 +0100

    Ajout d'un documentation basique. Correction du script sql.

--- BDD/init-dev.d/1-BBD-PositiceKIDS-tables.sql
+++ BDD/init-dev.d/1-BBD-PositiceKIDS-tables.sql
@@ -6,21 +6,19 @@ CREATE USER IF NOT EXISTS 'rootSQL'@'%' IDENTIFIED BY 'root';
 GRANT ALL PRIVILEGES ON positicekids . * TO 'rootSQL'@'%';
 
 CREATE TABLE `musee` (`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-    `nom` varchar(50) DEFAULT NULL,
-    `adresse` varchar(100) DEFAULT NULL,
-    `ville` varchar(20) DEFAULT NULL,
+    `nom` varchar(150) DEFAULT NULL,
+    `adresse` varchar(150) DEFAULT NULL,
+    `ville` varchar(60) DEFAULT NULL,
     `code_postal` int(5) DEFAULT NULL,
     `horaires` varchar(11) DEFAULT NULL,
     `tarif_reduit` float DEFAULT NULL,
     `tarif_plein` float DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `id` (`id`));
+    PRIMARY KEY (`id`));
 
 
 CREATE TABLE `theme` (`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
     `nom` varchar(50) DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `id` (`id`));
+    PRIMARY KEY (`id`));
 
 
 CREATE TABLE `exposition` (`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
@@ -32,7 +30,6 @@ CREATE TABLE `exposition` (`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
     `musee_id` bigint(20) unsigned DEFAULT NULL,
     `theme_id` bigint(20) unsigned DEFAULT NULL,
     PRIMARY KEY (`id`),
-    UNIQUE KEY `id` (`id`),
     KEY `lieu_id` (`musee_id`),
     KEY `theme_id` (`theme_id`),
     CONSTRAINT `fk_exposition_musee`
