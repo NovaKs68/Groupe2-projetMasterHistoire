c52ad6c8e829b43c90dfe851c90c1507290d973e
theo.lichtin <theo.lichtin@uha.fr>
2020-12-10 15:52:10 +0100

    essai affichage

---
+++ client/src/app/services/theme.service.ts
@@ -0,0 +1,33 @@
+import {Injectable} from "@angular/core";
+import {rejects} from "assert";
+
+@Injectable({
+  providedIn:'root'
+})
+
+export class ThemesService{
+
+  constructor() {}
+
+  public getAllThemes() : any {
+
+    return new Promise((resolve, reject) => {
+      fetch('http://localhost:8080/api/theme/', {
+        method: 'GET',
+        headers: { 'Content-type' : 'application/json; charset=UTF-8' }
+      })
+        .then((response) => {
+          response.json().then((data) =>{
+            console.log(data);
+            resolve(data);
+          });
+        })
+        .catch((err) => {
+          console.log('Fetch Error : ' , err);
+          reject(err);
+        });
+    });
+
+  }
+
+}
